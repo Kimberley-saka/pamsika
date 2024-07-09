8426045631107976cc41c9084e8298e177ae5b16
Kimberley-saka <kimberleysaka@gmail.com>
2024-07-09 18:18:06 +0200

    added other functions

--- users/models.py
+++ users/models.py
@@ -7,22 +7,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Users(AbstractUser):
-    """
-    user
-    """
-
-    password = models.CharField(max_length=255)
-
-
-    USERNAME_FIELD = 'email'
-
-
+    pass
 
 
 
     class Meta:
-        """
-        define table name
-        """
-
         db_table = 'users'
