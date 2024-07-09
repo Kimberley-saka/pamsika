eec1f86a6e848c82543f989900b33dabf24d9e14
Hope Mwafulirwa <47566478+Hope-Kagami@users.noreply.github.com>
2024-07-09 18:46:10 +0200

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-from django.db import models
-
-# Create your models here.
-=======
 """
 defines user class
 """
@@ -17,13 +12,10 @@ class Users(AbstractUser):
 
 
     class Meta:
-<<<<<<< HEAD
+
         db_table = 'users'
-=======
         """
         define table name
         """
 
         db_table = 'users'
->>>>>>> 57660b55b1c9146ca4428259680c3f0e31e2b370
->>>>>>> d7f273a07d20459610c54c481c3c1bb7af63d9e7
