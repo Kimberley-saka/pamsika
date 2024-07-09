9f39b5bcf72611d9ae6afb491bdd92ffab68f66a
Kimberley-saka <kimberleysaka@gmail.com>
2024-07-09 19:11:33 +0200

    edited db settings

--- users/models.py
+++ users/models.py
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-from django.db import models
-
-# Create your models here.
-=======
 """
 defines user class
 """
@@ -12,18 +7,29 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Users(AbstractUser):
-    pass
+    password = models.CharField(max_length=255)
 
+    USERNAME_FIELD = 'email'
+
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+    
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+   
+        return self.is_admin
 
 
     class Meta:
-<<<<<<< HEAD
         db_table = 'users'
-=======
-        """
-        define table name
-        """
-
-        db_table = 'users'
->>>>>>> 57660b55b1c9146ca4428259680c3f0e31e2b370
->>>>>>> d7f273a07d20459610c54c481c3c1bb7af63d9e7
