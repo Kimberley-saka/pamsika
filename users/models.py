ff23769785c760f615ff90574b188b2c3b4c7f41
Kimberley-saka <kimberleysaka@gmail.com>
2024-07-09 19:15:46 +0200

    solved merge conflict

--- users/models.py
+++ users/models.py
@@ -32,13 +32,10 @@ class Users(AbstractUser):
 
 
     class Meta:
-<<<<<<< HEAD
         db_table = 'users'
-=======
+
         """
         define table name
         """
 
         db_table = 'users'
->>>>>>> 57660b55b1c9146ca4428259680c3f0e31e2b370
->>>>>>> d7f273a07d20459610c54c481c3c1bb7af63d9e7
