120cb3c284fb7666565cad34456393b54514ba41
Hope-Kagami <hopekagami@outlook.com>
2024-07-15 23:09:32 +0200

    added a requirements file

--- users/migrations/0002_alter_users_email_alter_users_password.py
+++ users/migrations/0002_alter_users_email_alter_users_password.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0001_initial'),
+        ("users", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='users',
-            name='email',
+            model_name="users",
+            name="email",
             field=models.EmailField(max_length=254, unique=True),
         ),
         migrations.AlterField(
-            model_name='users',
-            name='password',
+            model_name="users",
+            name="password",
             field=models.CharField(max_length=255),
         ),
     ]
--- users/models.py
+++ users/models.py
@@ -1,20 +1,18 @@
 """
 defines user class
 """
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
 
 
 class Users(AbstractUser):
     password = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
 
+    USERNAME_FIELD = "email"
 
-    USERNAME_FIELD = 'email'
-
-    REQUIRED_FIELDS = ['firstname', 'lastname']
-
+    REQUIRED_FIELDS = ["firstname", "lastname"]
 
     def __str__(self):
         return self.email
@@ -25,20 +23,19 @@ class Users(AbstractUser):
 
     def has_module_perms(self, app_label):
         "Does the user have permissions to view the app `app_label`?"
-    
+
         return True
-    
+
     @property
     def is_staff(self):
         "Is the user a member of staff?"
         return self.is_admin
 
-
     class Meta:
-        db_table = 'users'
+        db_table = "users"
 
         """
         define table name
         """
 
-        db_table = 'users'
+        db_table = "users"
